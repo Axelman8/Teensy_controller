2deae11d480381354a77afc9ba985394990a81a4
Axelman8 <67999429+Axelman8@users.noreply.github.com>
2025-06-17 23:11:11 +0200

    .

--- display/ST7789Screen.cpp
+++ display/ST7789Screen.cpp
@@ -1,5 +1,5 @@
 #include "ST7789Screen.h"
-#include "../axe/AxeStructs.h"
+#include "../axefx/AxeStructs.h"
 
 ST7789Screen::ST7789Screen(uint8_t csPin, uint8_t dcPin, uint8_t rstPin)
   : _csPin(csPin), _dcPin(dcPin), _rstPin(rstPin), _needsFullRedraw(true), _contentType(0),
